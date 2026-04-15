b5c44269b538cf1f5ad322f029022c5f42bb4a17
jasperayawan <ejayawan22@gmail.com>
2026-04-15 16:22:38 +0800

    update frontend

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 $servername = getenv('DB_HOST') ?: 'sql100.infinityfree.com';
 $dbname   = getenv('DB_NAME') ?: 'if0_41667024_sharpcuts_db';
 $username = getenv('DB_USER') ?: 'if0_41667024';
-password = getenv('DB_PASS') ?: '1p9RoPSkK13BW';
+$password = getenv('DB_PASS') ?: '1p9RoPSkK13BW';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
